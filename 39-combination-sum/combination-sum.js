c7f737153350ae7b40a4a71d9b0459845e6d655b
Muhammed Sinan <mhdsinanat20@gmail.com>
2026-08-19 19:49:49 +0530

    Time: 5 ms (29.14%) | Memory: 60.2 MB (36.76%) - LeetSync

---
+++ 39-combination-sum/combination-sum.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+     const result = [];
+
+    function backtrack(start, current, sum) {
+        // Found a valid combination
+        if (sum === target) {
+            result.push([...current]);
+            return;
+        }
+
+        // Sum exceeded target
+        if (sum > target) {
+            return;
+        }
+
+        for (let i = start; i < candidates.length; i++) {
+            current.push(candidates[i]);
+
+            backtrack(
+                i,
+                current,
+                sum + candidates[i]
+            );
+
+            current.pop();
+        }
+    }
+
+    backtrack(0, [], 0);
+
+    return result;
+};
