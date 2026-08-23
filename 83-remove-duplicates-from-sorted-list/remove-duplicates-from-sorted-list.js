26702aeac170cd3773b63852ce9dd7452f5e1f3c
Muhammed Sinan <mhdsinanat20@gmail.com>
2026-08-23 14:43:57 +0530

    Time: 2 ms (7.69%) | Memory: 57.8 MB (53.08%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -10,15 +10,18 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
+    let seen  = new Set();
     let current = head;
-    
-    while (current !== null && current.next !== null) {
-        if (current.val === current.next.val) {
-            current.next = current.next.next;
-        } else {
-            current = current.next;
+    let prev = null;
+
+    while(current !==null){
+        if(seen.has(current.val)){
+            prev.next= current.next;
+        } else{
+            seen.add(current.val);
+            prev = current;
         }
+        current = current.next;
     }
-    
     return head;
 };
